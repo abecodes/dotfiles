fa69eab8856de69e794e0c118fc8ed6dca179274
abecodes <me@abe.codes>
2024-06-13 21:50:43 +0200

    leap styling

--- .config/nvim/lua/plugins/leap/setup.lua
+++ .config/nvim/lua/plugins/leap/setup.lua
@@ -1,3 +1,9 @@
 -- Or just set to grey directly, e.g. { fg = '#777777' },
 -- if Comment is saturated.
 -- vim.api.nvim_set_hl(0, 'LeapBackdrop', { link = 'Comment' })
+vim.api.nvim_set_hl(0, 'LeapLabelPrimary', {
+  link = 'Todo',
+})
+vim.api.nvim_set_hl(0, 'LeapLabelSecondary', {
+  link = 'IncSearch',
+})
