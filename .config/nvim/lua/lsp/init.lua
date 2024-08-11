f7b536ce025cc203696190673ba0c432424e49b9
abecodes <me@abe.codes>
2024-08-11 22:00:29 +0200

    adding clang and zig

--- .config/nvim/lua/lsp/init.lua
+++ .config/nvim/lua/lsp/init.lua
@@ -1,7 +1,18 @@
 local nvim_lsp = require('lspconfig')
 local custom_attach = require('lsp.utils.attach')
 
-local servers = { 'gopls', 'tsserver', 'efm', 'rust_analyzer', 'html' }
+local servers = {
+	-- INFO: Name needs to equal the name in
+	-- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
+	'clangd',
+	'efm',
+	'gopls',
+	'html',
+	'rust_analyzer',
+	'tsserver',
+	'zls',
+}
+
 for _, lsp in ipairs(servers) do
   nvim_lsp[lsp].setup(vim.tbl_extend(
 		'force',
---
+++ .config/nvim/lua/lsp/servers/clangd/settings.lua
@@ -0,0 +1,33 @@
+local nvim_lsp = require('lspconfig')
+local util = require('lspconfig.util')
+local hooks = require('hooks.hooks')
+
+hooks.register(
+	'*.c,*.cpp',
+	'BufWritePre',
+	{
+		function()
+			vim.lsp.buf.format(nil, 1000)
+		end
+	}
+)
+
+return {
+    filetypes = { 'c', 'cpp', 'objc', 'objcpp', 'cuda', 'proto' },
+    -- cmd = { '/usr/bin/clangd' },
+    -- '--log=verbose'}
+    cmd = {'/usr/bin/clangd', '--background-index', '--clang-tidy'},
+    single_file_support = true,
+    root_dir = util.root_pattern(
+        '.clangd',
+        '.clang-tidy',
+        'compile_commands.json',
+        'compile_flags.txt',
+        'configure.ac',
+        'build.zig',
+        '.git'
+    ),
+    -- initialization_options = {
+    --     fallback_flags = { '-std=c++17' },
+    -- },
+}
