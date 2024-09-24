27d60bfffb6bd4aedcface085ad208d93188344e
abecodes <me@abe.codes>
2024-09-24 09:24:30 +0200

    typo

--- .config/nvim/lua/plugins/leap/config.lua
+++ .config/nvim/lua/plugins/leap/config.lua
@@ -1,6 +1,6 @@
 -- require('leap').create_default_mappings()
 vim.keymap.set({'n', 'x', 'o'}, 's', '<Plug>(leap-forward)')
-vim.keymap.set({'n', 'x', 'o'}, 's', '<Plug>(leap-backward)')
+vim.keymap.set({'n', 'x', 'o'}, 'S', '<Plug>(leap-backward)')
 vim.keymap.set({'n', 'x', 'o'}, 'gs', '<Plug>(leap-from-window)')
 
 -- Define equivalence classes for brackets and quotes, in addition to
