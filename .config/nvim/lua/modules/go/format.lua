fe43014a66647eae85eab107281ec12fc67454b5
abecodes <me@abe.codes>
2025-01-29 09:52:39 +0100

    rewriting go formatting module

--- .config/nvim/lua/modules/go/format.lua
+++ .config/nvim/lua/modules/go/format.lua
@@ -1,14 +1,50 @@
 local utils = require('utils')
 
 return function()
+	local content = vim.api.nvim_buf_get_line(0, 0, -1, false)
 	local view = vim.fn.winsaveview()
+
 	-- breaking lines
-	vim.api.nvim_command(
-		"silent! exe 'undojoin | keepjumps :%! golines -m 80 -t 2 --no-chain-split-dots --base-formatter gofumpt 2>/dev/null < <(cat /dev/stdin) || cat /dev/stdin'"
-	)
+	-- { code = 0, signal = 0, stdout = 'hello', stderr = '' }
+	local res = vim.system(
+		{'golines', '-m', '80', '-t', '2', '--no-chain-split-dots', '--base-formatter', 'gofumpt'},
+		{
+			-- stdin = table.concat(content, '\n'),
+			stdin = content,
+			-- err: string, data: string (does not work in sync call aka :wait())
+			-- stdout = function(err, data)
+			-- end,
+			-- err: string, data: string (does not work in sync call aka :wait())
+			-- stderr = function(err, data)
+			-- end,
+			text = true
+		}
+	):wait()
+
+	if res.code == 0 then
+		content = vim.split(res.stdout, '\n')
+	end
+
 	-- organizing imports
-	vim.api.nvim_command(
-		"silent! exe 'undojoin | keepjumps :%! gci print -s standard -s default -s blank -s dot 2>/dev/null < <(cat /dev/stdin) || cat /dev/stdin'"
-	)
+	res = vim.system(
+		{'gci', 'print', '-s', 'standard', '-s', 'default', '-s', 'blank', '-s', 'dot'},
+		{
+			-- stdin = table.concat(content, '\n'),
+			stdin = content,
+			-- err: string, data: string (does not work in sync call aka :wait())
+			-- stdout = function(err, data)
+			-- end,
+			-- err: string, data: string (does not work in sync call aka :wait())
+			-- stderr = function(err, data)
+			-- end,
+			text = true
+		}
+	):wait()
+
+	if res.code == 0 then
+		content = vim.split(res.stdout, '\n')
+	end
+
+	vim.api.nvim_buf_set_lines(0, 0, -1, false, content)
 	vim.fn.winrestview(view)
 end
